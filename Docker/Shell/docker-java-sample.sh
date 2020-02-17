5f2fb86edeaebfe1cfa045998e05123b7721b583
Sebastian Colomar <SEBASTIAN.COLOMAR@GMAIL.COM>
2020-02-17 01:23:56 +0000

    revamp

--- Docker/Shell/docker-java-sample.sh
+++
@@ -1,4 +0,0 @@
-docker image build --tag docker-java-sample https://raw.githubusercontent.com/secobau/openshift/master/Docker/Dockerfile/docker-java-sample
-#docker image ls
-#docker image history docker-java-sample
-docker container run --rm --name docker-java-sample docker-java-sample
--- Docker/Shell/hellojava.sh
+++
@@ -1,5 +0,0 @@
-docker image build --tag hellojava https://raw.githubusercontent.com/secobau/openshift/master/Docker/Dockerfile/hellojava
-#docker image ls
-#docker image history hellojava
-docker container run hellojava
-docker container run hellojava -help
