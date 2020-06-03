8d57c52b75bd54cf8abcf464ea291e9fc921b1f4
secobau <sebastian.colomar@gmail.com>
2020-06-03 22:11:00 +0200

    Create fix-config.sh

---
+++ install/fix-config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+# ./install/fix-config.sh
+#########################################################################
+#      Copyright (C) 2020        Sebastian Francisco Colomar Bauza      #
+#      SPDX-License-Identifier:  GPL-2.0-only                           #
+#########################################################################
+file=install-config.yaml
