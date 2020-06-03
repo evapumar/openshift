218f572d0171eebdce9a4b7ca373690684969311
Sebastian Colomar <sebastian.colomar@gmail.com>
2020-06-03 22:30:19 +0200

    fix

--- install/fix-config.sh
+++ install/fix-config.sh
@@ -4,4 +4,12 @@
 #      Copyright (C) 2020        Sebastian Francisco Colomar Bauza      #
 #      SPDX-License-Identifier:  GPL-2.0-only                           #
 #########################################################################
-file=install-config.yaml
+file=install-config.yaml						;
+#########################################################################
+sed --in-place 								\
+	/' 'platform/d 							\
+	$file								;
+sed --in-place 								\
+	/'name.*master/s/^.*$/  name: master\n  platform:\n    aws:\n      type: 3a.xlarge'/ \
+	$file								;
+#########################################################################
