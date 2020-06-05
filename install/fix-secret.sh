7331fe59ede6d2e3d32e8dd7e37b006e77585488
secobau <sebastian.colomar@gmail.com>
2020-06-05 05:01:41 +0200

    Create fix-secret.sh

---
+++ install/fix-secret.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#	./install/fix-certificate.sh
+#########################################################################
+#      Copyright (C) 2020        Sebastian Francisco Colomar Bauza      #
+#      SPDX-License-Identifier:  GPL-2.0-only                           #
+#########################################################################
+crt=tls/fullchain1.pem							;
+key=tls/privkey1.pem							;
+crt=$( cat $crt | base64 --wrap 0 )					;
+key=$( cat $key | base64 --wrap 0 )					;
+kubectl get secret router-certs-default 				\
+	--namespace	openshift-ingress				\
+	--output	yaml						\
+	1>		router-certs-custom				;
+sed --in-place s/'  tls.crt: .*$'/"  tls.crt: $crt"/ router-certs-custom;
+sed --in-place s/'  tls.key: .*$'/"  tls.key: $key"/ router-certs-custom;
+kubectl delete secret router-certs-default --namespace openshift-ingress;
+kubectl apply --filename router-certs-custom				;
+kubectl delete pod --all --namespace openshift-ingress			;
+#########################################################################
